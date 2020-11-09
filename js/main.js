638559e913c26fd719baa63b2a7cfbdd6209c409
Khadija <khadija@curvetomorrow.com.au>
2020-11-09 21:35:49 +0530

    Implement media queries for different device sizes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
+// Initialize and add the map
 function initMap() {
-    const loc = { lat: 40.712776, lng: -74.005974 };
-
+    // Your location
+    const loc = { lat: 42.361145, lng: -71.057083 };
+    // Centered map on location
     const map = new google.maps.Map(document.querySelector('.map'), {
-        center: loc,
-        zoom: 8,
+        zoom: 14,
+        center: loc
     });
-    const marker = new google.maps.Marker({
-        position: loc,
-        map: map,
-    });
-} 
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
